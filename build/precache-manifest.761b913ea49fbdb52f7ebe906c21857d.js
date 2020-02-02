d2142e5116a29aec5dd0bd13fd6f7aef911b34ab
JoannaChang <joannahanchang@gmail.com>
2020-02-01 21:10:22 -0800

    try react

---
+++ build/precache-manifest.761b913ea49fbdb52f7ebe906c21857d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c00180f6563f04af1645a303d1c3b4a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b480ad8fbf6871d13f4",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "eca7a633c78a8a94dad3",
+    "url": "/static/js/2.7ca70303.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.7ca70303.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b480ad8fbf6871d13f4",
+    "url": "/static/js/main.2631a564.chunk.js"
+  },
+  {
+    "revision": "f8ce1a8cf35adf68175a",
+    "url": "/static/js/runtime-main.3b876b20.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
